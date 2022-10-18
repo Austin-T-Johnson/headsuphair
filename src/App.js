3af5927a0e0e59cc0ed6ee3579e7b105ecc9abc1
Austin Johnson <austintjohnson92@gmail.com>
2022-10-18 14:54:37 -0700

    landing page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.scss';
 import Header from './components/Header'
+import Landing from './components/Landing'
 function App() {
   return (
     <div className="App">
         <Header />
+        <Landing />
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Front from '../assets/images/front.webp'
+const Landing = () => {
+  return (
+    <div className='top-container'>
+        <div className='top-img-container'></div>
+        <div className='heading'>Heads Up Hair Designs And Salon Suites</div>
+        <div className='paragraph'>Your one stop shop for professional hair and beauty needs providing the Coachella Valley with expert service since 1979</div>
+    </div>
+  )
+}
+
+export default Landing
