93bb5d34f6364e1c14d164604e1448ad482fd93d
Austin Johnson <austintjohnson92@gmail.com>
2022-10-18 14:12:10 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from '../assets/images/logo.webp'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons'
+const Header = () => {
+    return (
+        <div className='header-container'>
+
+            <div className='contact-container'>
+                <div className='call-now'>
+                    <FontAwesomeIcon className='phone-icon' icon={faPhone} /> (760) 325-4247
+                </div>
+                <div className='logo-container'>
+                    <img src={Logo} alt='logo' className='logo'></img>
+                </div>
+                <div className='address'>
+                    <FontAwesomeIcon className='location-icon' icon={faLocationDot} />  1199 Ramon Rd Palm Springs, CA 92264
+                </div>
+            </div>
+            <div className='nav-container'>
+                <div>Home</div>
+                <div>About</div>
+                <div>Services</div>
+                <div>Contact</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
