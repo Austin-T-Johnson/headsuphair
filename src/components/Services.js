3f7207c45b37e38a608aaba59fdffb2fb773e994
Austin Johnson <austintjohnson92@gmail.com>
2023-01-16 14:05:22 -0800

    service.js refactor

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import Contact from "./Contact";
-import servicesPricing from "../JSON/services-pricing.json";
-import nailServices from "../JSON/nail-services.json";
 import servicesCard from "../JSON/services-card.json";
-import beautyServices from "../JSON/beauty-services.json"
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -15,142 +12,95 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Services = () => {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState({});
+  const [isExpanded, setIsExpanded] = useState(true);
 
   const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
     return <IconButton {...other} />;
   })(({ theme, expand }) => ({
-    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    transform: !expand ? "rotate(180deg)" : "rotate(0deg)",
     marginLeft: "auto",
     transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   }));
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const handleExpandClick = (id) => {
+    setExpanded((expanded) => ({
+      ...expanded,
+      [id]: !expanded[id],
+    }));
+    setIsExpanded(!isExpanded);
+    console.log(isExpanded);
   };
+
 
   return (
     <>
       <div className="background"></div>
       <div className="services-container">
-      <span className="services-header-txt">Our Services</span>
+        <span className="services-header-txt">Our Services</span>
         <div className="services-header-img"></div>
-        {/* <div className="services-info-container">
-          {servicesCard.map((data, idx) => {
+        <div className="services-card-container">
+          {servicesCard.map((service, index) => {
             return (
-              <div className="services-info" key={idx}>
-                <h1>{data.title}</h1>
-                <h2>{data.subtitle}</h2>
-                <div className="services-info-txt-container">
-                  <p className="services-info-p">{data.info}</p>
-                </div>
-                <h3>{data.subinfo}</h3>
+              <div className="services-card" key={index}>
+                <Card sx={{ maxWidth: 845 }}>
+                  <CardHeader title={service.title} />
+                  <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                      {service.info}
+                    </Typography>
+                  </CardContent>
+                  <CardActions disableSpacing>
+                    <ExpandMore
+                      expand={isExpanded}
+                      onClick={(id) => handleExpandClick(service.id)}
+                      aria-expanded={expanded}
+                      aria-label="show more"
+                    >
+                      <ExpandMoreIcon />
+                    </ExpandMore>
+                  </CardActions>
+                  <Collapse
+                    in={expanded[service.id]}
+                    timeout="auto"
+                    unmountOnExit
+                  >
+                    <CardContent>
+                      <div className="service-card-info-wrapper">
+                        <div className="service-card-text-left">
+                          {service.txt.map((txt) => {
+                            return (
+                              <Typography paragraph key={index}>{txt}</Typography>
+                            );
+                          })}
+                        </div>
+                        <div className="service-card-prices-right">
+                            {service.price.map((price) => {
+                                return (
+                                   <Typography paragraph key={index}>
+                            {price}
+                          </Typography>  
+                                )
+                            })}
+                         
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Collapse>
+                </Card>
               </div>
             );
           })}
-        </div> */}
-
-        <div className="services-card-container">
-          <div className="services-card">
-            <Card sx={{ maxWidth: 845 }}>
-              <CardHeader title="Hair Services" />
-              <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  We offer the largest selection of hair services in the
-                  Coachella Valley, click the dropdown below to see our full
-                  list.
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <ExpandMore
-                  expand={expanded}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </ExpandMore>
-              </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                  {servicesPricing.map((service) => {
-                    return <Typography paragraph>{service.txt}</Typography>;
-                  })}
-                </CardContent>
-              </Collapse>
-            </Card>
-          </div>
-
-          <div className="services-card">
-            <Card sx={{ maxWidth: 845 }}>
-              <CardHeader title="Nail Services" />
-              <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  Our licenced nail technician specializes in pedicures,
-                  manicures, nail art, and more. Click the dropdown to see a
-                  full list of services.
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <ExpandMore
-                  expand={expanded}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </ExpandMore>
-              </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                  {nailServices.map((service) => {
-                    return <Typography paragraph>{service.txt}</Typography>;
-                  })}
-                </CardContent>
-              </Collapse>
-            </Card>
-          </div>
-
-          <div className="services-card">
-            <Card sx={{ maxWidth: 845 }}>
-              <CardHeader title="Beauty Services" />
-              <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  Our beauty technician specializes in all your beauty needs. Click the dropdown to see a
-                  full list of services.
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <ExpandMore
-                  expand={expanded}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </ExpandMore>
-              </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                  {beautyServices.map((service) => {
-                    return <Typography paragraph>{service.txt}</Typography>;
-                  })}
-                </CardContent>
-              </Collapse>
-            </Card>
-          </div>
-
-         
         </div>
         <div className="disclaimer">
-            <h3>
-              Our prices vary depending upon the length of hair and level of
-              stylist. Prices subject to change without notice.
-            </h3>
-          </div>
+          <h3>
+            Our prices vary depending upon the length of hair and level of
+            stylist. Prices subject to change without notice.
+          </h3>
+        </div>
       </div>
 
       <Contact />
